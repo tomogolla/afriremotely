26f423b0675d5856556b90e09e7e372bb8e9905d
tomogolla <tomogolla637@gmail.com>
2025-09-20 18:40:46 +0300

    added render url to allowed host

--- afriremotely/settings.py
+++ afriremotely/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://afriremotely.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['afriremotely.onrender.com', 'https://afriremotely.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
