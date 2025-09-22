d329638b00eea2ba522ef16d66f2bd24b98b2072
tomogolla <tomogolla637@gmail.com>
2025-09-22 09:01:22 +0300

    Add home page template with Bootstrap styling and developer information

--- afriremotely/urls.py
+++ afriremotely/urls.py
@@ -17,13 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Job Board API",
+        default_version='v1',
+        description="API documentation for Job Board Backend",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('jobs.urls')),
     path('api/', include('api.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
     
 ]
